a7074146f9a2c051749835081ad227f77505f579
Andy <andrewpeliza@hotmail.com>
2018-12-09 13:02:01 +0000

    Day 2 Part 1

--- Day2.js
+++ Day2.js
@@ -1,18 +1,41 @@
-var lineReader = require('readline').createInterface({
+const lineReader = require('readline').createInterface({
     input: require('fs').createReadStream('./inputDay2.txt')
 });
 
-let lines = [];
+boxIds = [];
 
-counter = () => {
-    lineReader
-        .on('line', function (line) {
-            lines.push(line);
-        })
-        .on('close', () => {
-            
-            // console.log(lines);
-        });
+// Returns true if same letter appears n times, otherwise false
+const sameLetterAppearsNTimes = (str, n) => {
+    let result = false; 
+
+    str.split("").map(character => {
+        if(countCharacter(character, str) == n) {
+            result = true;
+        }
+    });
+
+    return result;
 }
 
-counter();
+// Returns number of occurrences of character in string
+const countCharacter = (character, str) => {
+    return str.match(new RegExp(character,"g")).length;
+}
+
+lineReader.on('line', line => {
+    boxIds.push(line);
+}).on('close', () => {
+
+    let appearsTwice = 0;
+    let appearsThreeTimes = 0;
+
+    for(id of boxIds) {
+        if(sameLetterAppearsNTimes(id, 2)) appearsTwice++;
+        if(sameLetterAppearsNTimes(id, 3)) appearsThreeTimes++;
+    }
+
+    console.log("Appears twice: " + appearsTwice);
+    console.log("Appears three times: " + appearsThreeTimes);
+    console.log("Total checksum: " + appearsTwice * appearsThreeTimes);
+      
+});
